27b663161319cea387cc3daec4b992c3cb774b6d
Kevin Fonteyne <kfonteyne@outlook.com>
2022-09-19 17:42:32 -0400

    assignment 1/2

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+"""Sample hello world Flask app"""
+
+
+
+# @app.route("/")
+# def hello():
+#     return "<h1>Hello, world!</h1>"
---
+++ run.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+"""Init script for app"""
+
+
+from app.routes import app
